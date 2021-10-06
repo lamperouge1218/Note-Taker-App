59a644fa4815d010ae678026fa7c471e9549129a
lamperouge <philbohn1791@gmail.com>
2021-10-06 17:47:17 -0400

    started coding the POST request. got data to append to the .json file though not within the array of the .json file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const notesDB = require("./db/db.json");
 const path = require('path');
 const fs = require("fs");
+const uuidv1 = require('uuidv1');
+
 const PORT = 3001;
 const app = express();
 
@@ -12,23 +14,52 @@ app.use(express.static('public'));
 // GET Requests as follows:
 
 // Index.html on main page load. No idea how GET * Works yet.
-// TODO: Make sure that GET * fucntions as needed in assignment README (10/6/2021)
+// TODO: (10/6/2021) Make sure that GET * fucntions as needed in assignment README 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+});
 
 // Notes.html upon button click and navigation to specific address
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html')); 
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// TODO: GET /api/notes should read the db.json file and return all saved notes as JSON
+// GET /api/notes reads the db.json file and returns all saved notes as JSON
 app.get('/api/notes', (req, res) => res.json(notesDB));
 
 // TODO: POST /api/notes should receive a new note to save on the request body, 
 // add it to the db.json file, and then return the new note to the client. 
 // Each note should have a unique ID.
+app.post("/api/notes", (req, res) => {
+    console.log(`${req.method} request received to add new note`);
+    // Destructuring for items 
+    const { title, text, id } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            review_id: uuidv1(),
+        };
+
+        const noteString = JSON.stringify(newNote);
+
+        fs.appendFile("./db/db.json", noteString, (err) =>
+            err ? console.error(err) : console.log(`Note ${newNote.title} has been written to JSON file`)
+        );
+
+        const response = {
+            status: "success",
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+})
 
 app.listen(PORT, () =>
-  console.log(`Express server listening on port ${PORT}!`)
+    console.log(`Express server listening on port ${PORT}!`)
 );
