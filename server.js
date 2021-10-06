2f61b358d7d56d3e79ed89192a97e069f8e83eb7
lamperouge <philbohn1791@gmail.com>
2021-10-06 16:50:13 -0400

    GET /api/notes returns saved notes from the db.json file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const notes = require("./db/db.json");
+const notesDB = require("./db/db.json");
 const path = require('path');
 const fs = require("fs");
 const PORT = 3001;
@@ -9,13 +9,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// GET Requests as follows:
+
+// Index.html on main page load
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-  })
+  });
 
+// Notes.html upon button click and navigation to specific address
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html')); 
-})
+});
+
+// TODO: GET /api/notes should read the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => res.json(notesDB));
+
+// TODO: POST /api/notes should receive a new note to save on the request body, 
+//add it to the db.json file, and then return the new note to the client. 
+//Each note should have a unique ID.
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
