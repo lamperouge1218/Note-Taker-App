161d5c2c35d30b919e5b872b1c4f643ae44caa09
lamperouge <philbohn1791@gmail.com>
2021-10-06 16:42:27 -0400

    got the /notes get returning the notes.html page

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const notes = require("./db/db.json");
+const path = require('path');
+const fs = require("fs");
 const PORT = 3001;
 const app = express();
-const notes = require("./db/db.json");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  })
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html')); 
+})
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
