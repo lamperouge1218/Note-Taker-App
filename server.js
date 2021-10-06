4f8677e35c7528b91b14fdccb37abb16bcd75489
lamperouge <philbohn1791@gmail.com>
2021-10-06 16:22:57 -0400

    added files from develop folder. made server.js file and added boilerplate server info

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const PORT = 3001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
